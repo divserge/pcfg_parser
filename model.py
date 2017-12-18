452a3a1049a850e03b81b6c9219af84e26790409
Sergei <Sergei.Divakov@skoltech.ru>
2017-12-18 15:41:09 +0300

    added E and M steps for parameter reestimation

--- model.py
+++ model.py
@@ -30,20 +30,36 @@ class PcfgParser:
 	def parse_tree(self, seq):
 
 		alpha, beta = inside_outside(self.__transform_text([seq])[0], self.T, self.Q)
-		mu = 
+		mu = alpha * beta
 
 
 	def collect_statistics(self, seq, alpha, beta):
-		pass
 		
+		inner_sum = np.einsum('rpd,sdq->rspq', beta[:, :, :-1], beta[:, 1:, :])
+
+		p, q = np.meshgrid(np.arange(beta.shape[1]), np.arange(beta.shape[1]))
+
+		numerator_zero_mask = np.where(q <= p)
+		denominator_zero_mask = np.where(q < p)
+
+		alpha_masked = deepcopy(alpha)
+		alpha_masked[:, denominator_zero_mask] = 0.
+
+		denominator = np.einsum('jpq,jpq->j', alpha_masked, beta)
+
+		alpha_masked[:, numerator_zero_mask] = 0.
+
+		numerator = np.einsum('jpq,jrs,rspq->jrs', alpha, self.T_data, inner_sum)
+
+		self.T_temp += numerator / denominator[:, np.newaxis, np.newaxis]
+		self.Q_temp[:, seq] += np.einsum('jhh,jhh->j', alpha, beta) / denominator
 
 	def recompute_parameters(self):
 		
-		self.T_temp = self.T_temp / self.T_temp.sum(axis = 0)[np.newaxis, :, :]
-		self.Q_temp = self.Q_temp / self.Q_temp.sum(axis = 1)[:, np.newaxis]
+		self.T_data = self.T_temp / (self.T_temp.sum(axis = [1, 2])[:, np.newaxis, np.newaxis] + self.Q_temp.sum(axis = 1)[:, np,newaxis, np.newaxis])
+		self.Q = self.Q_temp / (self.Q_temp.sum(axis = 1)[:, np.newaxis] + self.T_temp.sum(axis = [1, 2])[:, np.newaxis])
 
-		self.T = self.tensor_wrapper(self.T_temp, [0], [1, 2])
-		self.Q_temp = deepcopy(self.Q_temp)
+		self.T = self.tensor_wrapper(self.T_data, [0], [1, 2])
 
 		self.T_temp = np.zeros_like(self.T_temp)
 		self.Q_temp = np.zeros_like(self.Q_temp)
