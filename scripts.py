b580d598a250eda79b18d94a5e372bfec1358fa1
Sergei <Sergei.Divakov@skoltech.ru>
2017-12-21 17:40:45 +0300

    added scripts for conducting experiments

--- scripts.py
+++ scripts.py
@@ -1,3 +1,5 @@
+from pparser import Parser
+
 def parse_grammars(path):
 	grammars = {}
 	root_syms = {}
@@ -22,4 +24,35 @@ def parse_grammars(path):
 				grammars[fr].append((to, prob))
 
 	return grammars, root_syms
-	
+
+def tuple_to_str(inp_tuple):
+    out_string = "("
+    for elem in inp_tuple:
+        if type(elem) == str:
+            out_string += elem + " "
+        else:
+            out_string += tuple_to_str(elem)
+    out_string += ")"
+    return out_string
+
+
+def writeout_parses(parser, input_file, output_file):
+    with open(input_file, 'r') as fin, open(output_file, 'w') as fout:
+        k = 0
+        for l in fin:
+            sent = [w.split('^')[0] for w in l.split()[:-1]][:20]
+            line = ' '.join(sent)
+            kek = parser.parse(line)
+            k += 1
+            if k % 100 == 0:
+                fout.flush()
+            print(tuple_to_str(kek), file=fout)
+
+
+def test_parser(approximation, rules_path, rank, input_file, output_folder):
+    rules, root = parse_grammars(path=rules_path)
+    p = Parser(rules, root, approximation=approximation, rank=rank)
+    writeout_parses(
+        input_file,
+        '{}/test_{}_{}.txt'.format(output_folder, approximation, rank)
+    )
